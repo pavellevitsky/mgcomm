945411b9cb642d4c079adf12b324f8fcba8a77c7
pavel levitsky <pavellev@inter.net.il>
2021-11-09 17:44:28 +0200

    Open terminal with neighbour cells info and paging

--- mgcomm.sh
+++ mgcomm.sh
@@ -104,13 +104,15 @@ then
   exit 1
 fi
 
-dciscan_enabled=`adb shell ls -l /system/bin/dciscan | grep "No such file or directory" | wc -l`
-webs_enabled=`adb shell ls -l /data/local/tmp/webs | grep "No such file or directory" | wc -l`
-at_cmd_enabled=`adb shell ls -l /data/local/tmp/ut-ModemAt | grep "No such file or directory" | wc -l`
+read_diag_ok=`adb shell ls -l /system/bin/read_diag | grep "No such file or directory" | wc -l`
+dciscan_ok=`adb shell ls -l /system/bin/dciscan | grep "No such file or directory" | wc -l`
+webs_ok=`adb shell ls -l /data/local/tmp/webs | grep "No such file or directory" | wc -l`
+at_cmd_ok=`adb shell ls -l /data/local/tmp/ut-ModemAt | grep "No such file or directory" | wc -l`
 
-if [[ $dciscan_enabled != 0 || $webs_enabled != 0 || $at_cmd_enabled != 0 ]]
+if [[ $read_diag_ok != 0 || $dciscan_ok != 0 || $webs_ok != 0 || $at_cmd_ok != 0 ]]
 then
-  echo One of below required components is missing:
+  echo One of required components is missing:
+  echo "- read_diag  @ /system/bin/"
   echo "- dciscan    @ /system/bin/"
   echo "- webs       @ /data/local/tmp/"
   echo "- ut-ModemAt @ /data/local/tmp/"
@@ -166,6 +168,8 @@ do
   echo '**  7  Set LTE preffered network type               **'
   echo '**  8  Trigger power scan                           **'
   echo '**  9  Camp on LTE cell (switch to LTE before)      **'
+  echo '**  N  Show neighbour cells (WCDMA/LTE)             **'
+  echo '**  P  Paging signals                               **'
   echo '** ------------------------------------------------ **'
   echo '**  UL transmission configuration                   **'
   echo '** ------------------------------------------------ **'
@@ -196,7 +200,9 @@ do
        read -p "Press ENTER to continue ..."
        ;;
     C) send_at_mgphycfg;;
-    *) echo Select 0..9, A, B, C;;
+    N) x-terminal-emulator -e "adb shell 'read_diag --events 0 --msgs 0:0x0 --logs 0x4111,0x41AC,0xB031,0xB119,0xB197'";;
+    P) x-terminal-emulator -e "adb shell 'read_diag --events 0 --msgs 0:0x0 --logs 0xB0CB'";;
+    *) echo Select 0..9, A, B, C, N, P;;
   esac
 done
 
