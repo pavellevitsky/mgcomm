01f37b5c1cc127e966fb533e5976584964db4c56
pavel levitsky <pavellev@inter.net.il>
2022-01-16 16:24:23 +0200

    [style] Reformat output

--- dci/DCIBase.cpp
+++ dci/DCIBase.cpp
@@ -12,67 +12,65 @@
 
 int DCIBase::set_log_codes(const std::unordered_set<uint16_t> &log_codes)
 {
-	int result;
-	result = reset_log_codes();
-	if(result)
-		return result;
+    int result;
+    result = reset_log_codes();
+    if(result)
+        return result;
 
-	return add_log_codes(log_codes);
+    return add_log_codes(log_codes);
 }
 
 int DCIBase::add_log_codes(const std::unordered_set<uint16_t> &log_codes)
 {
-	std::cout << '\n' << __func__ << ":\n\tAdding log codes to catch:";
-	for (auto x: log_codes)
-		printf(" 0x%.02hX", x);
-	return _manipulate_log_codes(log_codes, ENABLE);
+    std::cout << __func__ << " -> add logs -> ";
+
+    for (auto x: log_codes)
+        printf("0x%.02hX ", x);
+    printf("\n");
+
+    return _manipulate_log_codes(log_codes, ENABLE);
 }
 
 int DCIBase::reset_log_codes()
 {
-	std::cout << '\n' << __func__ << ":\n\tStopping to catch logs";
-	return _manipulate_log_codes(_log_codes, DISABLE);
+    std::cout << __func__ << " -> reset logs\n";
+    return _manipulate_log_codes(_log_codes, DISABLE);
 }
 
 int DCIBase::_manipulate_log_codes(const std::unordered_set<uint16_t> &log_codes, int action )
 {
-	std::cout << '\n' << __func__ << ":\n\t";
+    std::cout << __func__ << " -> ";
 
-	if(0 == log_codes.size())
-	{
-		std::cout << "nothing to do, empty set";
-		return 0;
-	}
+    if(0 == log_codes.size())
+    {
+        std::cout << "empty set\n";
+        return 0;
+    }
 
-	std::cout << (action == ENABLE ? "Adding logs": "Removing logs");
+    std::cout << (action == ENABLE ? "add logs -> " : "remove logs -> ");
 
-	for (auto x: log_codes)
-		printf(" 0x%.02hX", x);
+    for (auto x: log_codes)
+        printf("0x%.02hX ", x);
+    printf("\n");
 
-	// Configure device
-	if(_dci_configure_logs(action, log_codes))
-		return 1;	// Error
+    // Configure device
+    if(_dci_configure_logs(action, log_codes))
+        return 1;    // Error
 
-	// Modify active Log Codes
-	if(action == ENABLE)
-		_log_codes.insert(log_codes.begin(), log_codes.end());
-	else
-		_log_codes.erase(log_codes.begin(), log_codes.end());
+    // Modify active Log Codes
+    if(action == ENABLE)
+        _log_codes.insert(log_codes.begin(), log_codes.end());
+    else
+        _log_codes.erase(log_codes.begin(), log_codes.end());
 
-	std::cout << "\n\t\tNew log codes are: ";
-	for (auto x: _log_codes)
-		printf(" 0x%.02hX", x);
-
-	return 0;
+    return 0;
 }
 
 int DCIBase::set_event_codes(const std::unordered_set<int> &codes)
 {
-	(void) codes;
-
-	std::cout << "\nDCI::set_event_codes: Not implemented";
-
-	return 0;
+    (void) codes;
+    std::cout << "DCI::set_event_codes: not implemented\n";
+    return 0;
 }
 
 /*!
@@ -80,17 +78,17 @@ int DCIBase::set_event_codes(const std::unordered_set<int> &codes)
  */
 void DCIBase::_handle_log(unsigned char *ptr, int len)
 {
-	uint16_t log_code = *(uint16_t*)(ptr + 2);
+    uint16_t log_code = *(uint16_t*)(ptr + 2);
 
-	// Software simulation of log settings
-	auto match = _log_codes.find(log_code);
-	if(match != _log_codes.end())
-	{
-		printf("\nReceived log 0x%.04hX", log_code);
+    // Software simulation of log settings
+    auto match = _log_codes.find(log_code);
+    if(match != _log_codes.end())
+    {
+        printf("[0x%.04hX] -> ", log_code);
 
-		for(auto handler: _log_handlers)
-			handler(ptr, len);
-	}
+        for(auto handler: _log_handlers)
+            handler(ptr, len);
+    }
 }
 
 /*!
@@ -98,12 +96,12 @@ void DCIBase::_handle_log(unsigned char *ptr, int len)
  */
 void DCIBase::_handle_event(unsigned char *ptr, int len)
 {
-	(void)ptr;
-	(void)len;
+    (void)ptr;
+    (void)len;
 }
 
 void DCIBase::add_log_handler(log_callback_t const & c)
 {
-	//_log_handlers.insert(c);
-	_log_handlers.push_back(c);
+    //_log_handlers.insert(c);
+    _log_handlers.push_back(c);
 }
